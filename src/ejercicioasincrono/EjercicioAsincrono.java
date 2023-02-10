b752b75a045dd8ec5e6b4acfae7d7073f824ba70
Diego Antonio Sandoval Coto <diegoasc.2000@gmail.com>
2023-02-09 20:56:51 -0600

    subiendo archivo de ejercicio asincrono

---
+++ src/ejercicioasincrono/EjercicioAsincrono.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicioasincrono;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author diego
+ */
+public class EjercicioAsincrono {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        Scanner reader = new Scanner(System.in);
+        
+        int notaAlumn;
+        
+        System.out.println("Ingrese la nota del alumno");
+        notaAlumn = reader.nextInt();
+
+        if (notaAlumn >= 7.0 ) 
+            {
+                System.out.println("El alumno a aprobado la Materia");
+            }
+            else
+              if(notaAlumn <6.49)
+            {
+               System.out.println("El alumno a Reprobado la Materia"); 
+            }
+        if(notaAlumn >=6.5 && notaAlumn <=6.99)
+            {
+                System.out.println("Tiene la posibilidad de realizar un examen de suficiencia para aprobar.");
+            }
+        }
+    }    
